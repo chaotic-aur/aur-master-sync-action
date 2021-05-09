09b2129d7c4e10aff62da6413e81754387a75903
PedroHLC <root@pedrohlc.com>
2021-05-09 13:50:10 -0300

    Make it AUR-only, automate getting URL

--- mirror.sh
+++ mirror.sh
@@ -3,6 +3,6 @@ set -eu
 
 /setup-ssh.sh
 
-export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
-git remote add mirror "$INPUT_TARGET_REPO_URL"
-git push --tags --force --prune mirror "refs/remotes/origin/*:refs/heads/*"
+export GIT_SSH_COMMAND='ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no'
+git remote add aur "ssh://aur@aur.archlinux.org/$(sed -nE '/pkgbase/s/pkgbase = (.+)$/\1/p' .SRCINFO).git"
+git push aur master
