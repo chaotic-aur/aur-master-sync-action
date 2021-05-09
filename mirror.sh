0021d76654e6a546f78e3b2088d10510893baf4f
PedroHLC <root@pedrohlc.com>
2021-05-09 14:31:09 -0300

    Put _PKGBASE in a separate variable

--- mirror.sh
+++ mirror.sh
@@ -3,6 +3,8 @@ set -eu
 
 /setup-ssh.sh
 
+_PKGBASE="$(sed -nE '/pkgbase/s/pkgbase = (.+)$/\1/p' .SRCINFO)"
+
 export GIT_SSH_COMMAND='ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no'
-git remote add aur "ssh://aur@aur.archlinux.org/$(sed -nE '/pkgbase/s/pkgbase = (.+)$/\1/p' .SRCINFO).git"
+git remote add aur "ssh://aur@aur.archlinux.org/${_PKGBASE}.git"
 git push aur master
